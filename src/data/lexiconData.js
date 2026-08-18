9ae2d0fd124ff053c5ac1ab77b90dd5f4e7772ca
LPSmaragd <momomrf982@gmail.com>
2026-08-18 02:16:24 +0200

    Add Lexicon data

---
+++ src/data/lexiconData.js
@@ -0,0 +1,74 @@
+export const lexiconData = [
+  {
+    id: 1,
+    deutsch: 'Femur',
+    latin: 'Femur',
+    definition: 'Der Oberschenkelknochen ist der längste und stärkste Knochen des menschlichen Körpers.',
+    funktion: 'Trägt das Körpergewicht und ermöglicht Bewegungen der Hüfte und des Knies.',
+    innervation: 'Nervus femoralis (L2-L4)',
+    klinischRelevanz: 'Frakturen des Femurs sind schwerwiegend und können zu Morbidität führen. Oberschenkelhalsbrüche sind häufig bei älteren Menschen.'
+  },
+  {
+    id: 2,
+    deutsch: 'Tibia',
+    latin: 'Tibia (Schienbein)',
+    definition: 'Das Schienbein ist der mediale (innere) und größere der beiden Unterschenkelknochen.',
+    funktion: 'Trägt Gewicht und verbindet die Oberschenkelknochen mit dem Fuß.',
+    innervation: 'Nervus tibialis',
+    klinischRelevanz: 'Schienbeinkantensyndrom ist eine häufige Verletzung bei Läufern.'
+  },
+  {
+    id: 3,
+    deutsch: 'Humerus',
+    latin: 'Humerus (Oberarmknochen)',
+    definition: 'Der Oberarmknochen verbindet die Schulter mit dem Unterarm.',
+    funktion: 'Ermöglicht Schulterbewegungen und trägt zum Arm-Bewegungsradius bei.',
+    innervation: 'Nervus radialis, Nervus ulnaris, Nervus medianus',
+    klinischRelevanz: 'Oberarmfrakturen können die Nerven und Blutgefäße beschädigen.'
+  },
+  {
+    id: 4,
+    deutsch: 'Bizeps',
+    latin: 'Musculus biceps brachii',
+    definition: 'Ein großer Skelettmuskel an der Vorderseite des Oberarms mit zwei Ursprüngen.',
+    funktion: 'Hauptsächlich für die Flexion des Unterarms im Ellbogengelenk verantwortlich.',
+    innervation: 'Nervus musculocutaneus',
+    klinischRelevanz: 'Bizepssehnen-Verletzungen sind häufig bei überkopf-lastigen Aktivitäten.'
+  },
+  {
+    id: 5,
+    deutsch: 'Herz',
+    latin: 'Cor',
+    definition: 'Ein muskulöses, hohles Organ, das Blut durch den Körper pumpt.',
+    funktion: 'Pumpt sauerstoffreiches Blut zu allen Körpergeweben und nimmt sauerstoffarmes Blut auf.',
+    innervation: 'Sympathisches und parasympathisches Nervensystem',
+    klinischRelevanz: 'Herzerkrankungen sind die Haupttodesursache weltweit. Regelmäßiges Training unterstützt die Herzgesundheit.'
+  },
+  {
+    id: 6,
+    deutsch: 'Lunge',
+    latin: 'Pulmo',
+    definition: 'Ein Paar großer, schwammiger Organe, in denen der Gasaustausch stattfindet.',
+    funktion: 'Ermöglicht den Austausch von Sauerstoff und Kohlendioxid zwischen Blut und Luft.',
+    innervation: 'Vagus (Nervus vagus) und Sympathische Nerven',
+    klinischRelevanz: 'Lungenerkrankungen wie COPD und Asthma beeinflussen die Atmung erheblich.'
+  },
+  {
+    id: 7,
+    deutsch: 'Nieren',
+    latin: 'Renes',
+    definition: 'Ein Paar bohnenförmiger Organe, die Abfallprodukte aus dem Blut filtern.',
+    funktion: 'Filtert Blut, produziert Urin und reguliert Wasser- und Elektrolytehaushalt.',
+    innervation: 'Nervus renalis (sympathisch)',
+    klinischRelevanz: 'Nierenerkrankungen können zu Niereninsuffizienz führen und sind ein Grund für die Dialyse.'
+  },
+  {
+    id: 8,
+    deutsch: 'Rückenmark',
+    latin: 'Medulla spinalis',
+    definition: 'Ein zylindrischer Nervenstrang, der sich durch den Wirbelkanal erstreckt.',
+    funktion: 'Überträgt Signale zwischen dem Gehirn und dem Rest des Körpers; verfügbar für Reflexe.',
+    innervation: 'Zentral für das Nervensystem',
+    klinischRelevanz: 'Rückenmarksverletzungen können zu Lähmung führen. Die Höhe der Verletzung bestimmt den Grad der Auswirkungen.'
+  }
+]
